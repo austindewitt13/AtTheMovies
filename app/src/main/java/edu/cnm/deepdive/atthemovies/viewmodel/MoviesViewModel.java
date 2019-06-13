41153ab709cbb5fcfb6bdceba13915181c3ea013
austindewitt13 <50061410+austindewitt13@users.noreply.github.com>
2019-06-13 10:38:57 -0600

    added sql lite

--- app/src/main/java/edu/cnm/deepdive/atthemovies/viewmodel/MoviesViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/atthemovies/viewmodel/MoviesViewModel.java
@@ -1,40 +1,26 @@
 package edu.cnm.deepdive.atthemovies.viewmodel;
 
-
     import android.content.Context;
-    import androidx.lifecycle.MutableLiveData;
     import androidx.lifecycle.ViewModel;
+    import edu.cnm.deepdive.atthemovies.MoviesLiveData;
     import edu.cnm.deepdive.atthemovies.model.Movie;
-    import edu.cnm.deepdive.atthemovies.service.FileService;
 
-    import java.util.HashMap;
-    import java.util.Map;
+    public class MoviesViewModel extends ViewModel {
 
-public class MoviesViewModel extends ViewModel {
+  private MoviesLiveData movies;
 
-  private MutableLiveData<Map<Long,Movie>> movies;
-
-  public MutableLiveData<Map<Long,Movie>> getMoviesLiveData(Context context) {
+  public MoviesLiveData getMoviesLiveData(Context context) {
     if (movies == null){
-      movies = new MutableLiveData<>();
-      FileService.getInstance(context).getFromFile(movies);
+      movies = new MoviesLiveData(context.getApplicationContext());
     }
     return movies;
   }
 
   public void addMovie(Movie movie, Context context){
-    final Map<Long, Movie> movieMap = getMoviesLiveData(context).getValue();
-    movieMap.put(movie.getId(),movie);
-    getMoviesLiveData(context).setValue(movieMap);
-  }
-
-  public void saveData(Context context){
-    FileService.getInstance(context).writeToFile(getMoviesLiveData(context).getValue());
+   getMoviesLiveData(context).addMovie(movie);
   }
 
   public Movie getMovie(Long id, Context context){
     return getMoviesLiveData(context).getValue().get(id);
   }
 }
-
-
